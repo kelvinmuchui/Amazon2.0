734aa68cab93288b7e7da2e2a3c03bc724b22ae1
kelvinmuchui <muchuikelvin423@gmail.com>
2021-06-27 11:22:29 +0300

    add checkout page

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyCENU7C6hf2FPqh9a-SpMs9uayN_U49_vY",
+    authDomain: "fir-c1bcb.firebaseapp.com",
+    projectId: "fir-c1bcb",
+    storageBucket: "fir-c1bcb.appspot.com",
+    messagingSenderId: "407181050598",
+    appId: "1:407181050598:web:84d700f18d26e8b4bef127"
+  };
